97d15d7045123cb7e25702c1372fd7368f7102f4
alde103 <aldebaran_103@hotmail.com>
2020-07-27 22:32:57 -0500

    Initial commit

---
+++ lib/ty_loader.ex
@@ -0,0 +1,18 @@
+defmodule TyLoader do
+  @moduledoc """
+  Documentation for `TyLoader`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TyLoader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ty_loader_test.exs
@@ -0,0 +1,8 @@
+defmodule TyLoaderTest do
+  use ExUnit.Case
+  doctest TyLoader
+
+  test "greets the world" do
+    assert TyLoader.hello() == :world
+  end
+end
